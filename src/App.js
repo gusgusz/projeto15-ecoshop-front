0b0acf320e68cb982be287bfb791ac86b2b31773
talles-alexandre <alexandretalles13@gmail.com>
2022-11-27 18:28:15 -0300

    addCart

--- src/App.js
+++ src/App.js
@@ -4,18 +4,47 @@ import SignIn from "./pages/SignIn";
 import Products from "./pages/Products";
 import { useState } from "react";
 import { createContext } from "react";
+import HomePage from "./pages/HomePage";
 
 export const authContext = createContext();
 export default function App() {
   const [auth, setAuth] = useState(null);
+  const [visibility, setVisibility] = useState("hidden");
+  const [cartItens, setCartItens] = useState(
+    () => JSON.parse(localStorage.getItem("cart")) ?? []
+  );
 
   return (
     <BrowserRouter>
-      <authContext.Provider value={{ auth, setAuth }}>
+      <authContext.Provider
+        value={{
+          auth,
+          setAuth,
+          visibility,
+          setVisibility,
+          cartItens,
+          setCartItens,
+        }}
+      >
         <Routes>
-          <Route path="/" element={<SignUp />} />
+          <Route
+            path="/"
+            element={
+              <HomePage
+                cartItens={cartItens}
+                setCartItens={setCartItens}
+                setVisibility={setVisibility}
+              />
+            }
+          />
+          <Route
+            path="/products"
+            element={
+              <Products cartItens={cartItens} setCartItens={setCartItens} />
+            }
+          />
           <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/products" element={<Products />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </authContext.Provider>
     </BrowserRouter>
---
+++ src/pages/HomePage.js
@@ -0,0 +1,175 @@
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import Url from "../services/Api.js";
+import axios from "axios";
+
+export default function HomePage({ cartItens, setCartItens }) {
+  const [listProducts, setListProducts] = useState([]);
+
+  useEffect(() => {
+    const promise = axios.get(`${Url}/products`);
+
+    promise.then((res) => {
+      console.log(res.data.products);
+      setListProducts(res.data.products);
+    });
+
+    promise.catch((err) => {
+      console.log(err.response.data);
+    });
+  }, []);
+
+  function addCart() {}
+
+  return (
+    <Content>
+      <header>
+        <div className="logo">
+          <Eco>ECO</Eco>
+          <Shop>SHOP</Shop>
+        </div>
+
+        <div className="right">
+          <div className="cart">
+            <ion-icon
+              onClick={() => {
+                console.log("hi");
+              }}
+              name="cart-outline"
+            ></ion-icon>{" "}
+            <p>{cartItens.length > 0 ? cartItens.length : ""}</p>
+          </div>
+          <div className="menu">
+            <ion-icon
+              onClick={() => {
+                console.log("hi");
+              }}
+              name="menu-outline"
+            ></ion-icon>
+          </div>
+        </div>
+      </header>
+      <ListContainer>
+        {listProducts.map((p) => (
+          <div key={p._id}>
+            <img src={p.img} alt="imagem" />
+            <span> {p.describe}</span>
+            <p>R$ {p.price}</p>
+            <button
+              onClick={() => {
+                setCartItens([
+                  ...cartItens,
+
+                  {
+                    _id: p._id,
+                    name: p.name,
+                    img: p.img,
+                    price: p.price,
+                  },
+                ]);
+              }}
+            >
+              Adicionar ao Carrinho
+            </button>
+          </div>
+        ))}
+      </ListContainer>
+    </Content>
+  );
+}
+
+const Content = styled.div`
+  width: 100%;
+
+  header {
+    width: 100%;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #34d97e;
+  }
+
+  p {
+    font-size: 14px;
+  }
+  .logo {
+    display: flex;
+    align-items: center;
+  }
+
+  .right {
+    display: flex;
+    align-items: center;
+    font-size: 28px;
+    opacity: 0.9;
+    font-weight: 600;
+    gap: 15px;
+    cursor: pointer;
+    ion-icon[name="cart-outline"] {
+      cursor: pointer;
+    }
+    ion-icon[name="menu-outline"] {
+      cursor: pointer;
+    }
+  }
+  .cart {
+    display: flex;
+    align-items: center;
+  }
+  .menu {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Eco = styled.h1`
+  font-size: 19px;
+  color: green;
+
+  font-family: "Bungee Shade", cursive;
+`;
+const Shop = styled.h1`
+  font-size: 22px;
+  color: brown;
+`;
+const ListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 3px;
+  box-sizing: border-box;
+
+  div {
+    box-sizing: border-box;
+    background-color: #057a55;
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    align-items: center;
+    justify-content: flex-end;
+    width: 300px;
+    height: 300px;
+  }
+  div img {
+    width: 100px;
+    height: 100px;
+  }
+  div p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  div span {
+  }
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100px;
+    height: 50px;
+    border-radius: 5px;
+    margin-bottom: 0px;
+  }
+`;
